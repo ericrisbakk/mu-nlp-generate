3dece237585870f4e633ef4fbe6be3648e29ac5a
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2019-05-24 16:50:46 +0200

    Had to accommodate for the fact that csv saves lists as strings. ARGH!

--- CleanData.py
+++ CleanData.py
@@ -14,12 +14,6 @@ import pandas as pd
 import re
 from spellchecker import SpellChecker
 
-# Load data.
-data = pd.read_csv("LocalData/SongData.csv")
-print("Loaded dataset.")
-# Methods for preprocessing. These methods
-# are applied to the entire sentence.
-
 # Not used, currently.
 def add_start_and_end_symbols(sentence):
     return ("<start> " + sentence + " <stop>")
@@ -30,17 +24,30 @@ def basic_cleaning(sentence):
     return s
    # The below removes anything except whitespace and alphanumeric characters.
 def remove_punctuation(s):
-    return re.sub('[^\w\s]', ' ', s)
-
-# There are, to my awareness, no words with consecutive 
-# three same letters in english.
-def remove_extra_letters(s):
-    return re.sub(r"(.)\1{2,}", r"\1"*2, s)    
+    return re.sub('\\.|[^\w\s]', ' ', s)
 
 # Turn sentence into list of words.
 def tokenize(s):
     s_list = [w for w in s.split(' ') if w.strip() != '' or w == '\n']      
     return s_list
+
+# There are, to my awareness, no words with consecutive 
+# three same letters in english.
+def remove_extra_letters(s):
+    return re.sub(r"(.)\1{2,}", r"\1"*2, s)   
+
+# Replaces unknown words in list according to u_dict (given later)
+def replace_unknown(s_list):
+    for i, s in enumerate(s_list):
+        if s in u_dict:
+            s_list[i] = u_dict[s]
+    return s_list
+
+# Load data.
+data = pd.read_csv("LocalData/SongData.csv")
+print("Loaded dataset.")
+# Methods for preprocessing. These methods
+# are applied to the entire sentence. 
 
 print("Starting to clean and tokenize.")
 # Clean the data
@@ -50,12 +57,18 @@ data['clean'] = data['clean'].apply(remove_punctuation)
 print("Punctuation removed.")
 data['clean'] = data['clean'].apply(remove_extra_letters)
 print("Extra letters removed.")
-data['token'] = data['clean'].apply(tokenize)
+token_data = data['clean'].apply(tokenize)
 print("Strings tokenized.")
+
+print("\n", repr(data.text[0]), "\n")
+print("\n", repr(data.text[0][30:50]), "\n")
+print("\n", repr(data.clean[0]), "\n")
+print("\n", repr(data.clean[0][30:50]), "\n")
+
 
 # get unique tokens.
 print("Getting unique tokens.")
-text = data['token'].values
+text = token_data.values
 tokens = []
 for song in text:
     tokens.extend(song)
@@ -90,15 +103,18 @@ for u in unknown:
 u_list = list(set(u_list))
 u_dict = dict(u_list)
 
-def replace_unknown(s_list):
-    for i, s in enumerate(s_list):
-        if s in u_dict:
-            s_list[i] = u_dict[s]
-    return s_list
-
 print("Creating column of 'corrected' spellings")
-data['corrected'] = data.token.apply(replace_unknown)
+corrected_data = token_data.apply(replace_unknown)
 
+print("converted tokenized corrected spellings back to string")
+def convert_s_list_to_string(s_list):
+    return " ".join(s_list)
+
+data['corrected'] = corrected_data.apply(convert_s_list_to_string)
+
+print("\n", repr(data.corrected[0]))
+
+print("\nSaving CSV file")
 data.to_csv("LocalData/ProcessedSongData.csv")
 print("CSV file saved.")
 
